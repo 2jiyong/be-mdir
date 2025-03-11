4bc8a73a0d15e9e59030b10a7dd7ab777f99f5e5
2jiyong <jdragon2000@naver.com>
2025-03-11 15:29:10 +0900

    Feat: append해서 write 하는 기능 추가

--- src/main/java/org/writer/TextWriter.java
+++ src/main/java/org/writer/TextWriter.java
@@ -7,15 +7,24 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class TextWriter {
-    private final String path;
+    private final String writePath;
 
-    public TextWriter(String path) {
-        this.path = path;
+    public TextWriter(String writePath) {
+        this.writePath = writePath;
     }
 
     public void write(String title, TextContent textContent) {
-        String filePath = path+"/"+title+".txt";
+        String filePath = writePath +"/"+title+".txt";
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
+            writer.write(textContent.getText());
+        } catch (IOException e) {
+            return;
+        }
+    }
+
+    public void appendWrite(String title, TextContent textContent) {
+        String filePath = writePath +"/"+title+".txt";
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath,true))) {
             writer.write(textContent.getText());
         } catch (IOException e) {
             return;
