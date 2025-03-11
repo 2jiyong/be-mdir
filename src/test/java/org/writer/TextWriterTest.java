e07206f3a1f434543691b19f893e7af12c55dc62
2jiyong <jdragon2000@naver.com>
2025-03-11 15:28:32 +0900

    Test: TextWriter appendTest 추가

--- src/test/java/org/writer/TextWriterTest.java
+++ src/test/java/org/writer/TextWriterTest.java
@@ -10,15 +10,28 @@ import java.util.Optional;
 
 public class TextWriterTest {
     TextReader reader = new TextReader();
+    String path = "files";
+    TextWriter writer = new TextWriter(path);
 
     @Test
     @DisplayName("TextWriter가 해당 경로에 TextContent의 내용을 담은 파일을 만들 수 있다.")
     void testTextWriter() {
-        String path = "files";
         String title = "writer";
         TextContent textContent = new TextContent("hello writer");
-        TextWriter textWriter = new TextWriter(path);
-        textWriter.write(title,textContent);
+        writer.write(title,textContent);
         assertThat(reader.read(path+"/"+title+".txt").get().getText()).isEqualTo("hello writer");
+    }
+
+    @Test
+    @DisplayName("해당 경로에 이미 파일이 있다면, 내용을 추가할 수 있다.")
+    void testTextWriterAppend() {
+        String title = "append";
+        // 파일 추가
+        TextContent textContent = new TextContent("hello append1");
+        writer.write(title,textContent);
+
+        TextContent appendText = new TextContent(" append2");
+        writer.appendWrite(title,appendText);
+        assertThat(reader.read(path+"/"+title+".txt").get().getText()).isEqualTo("hello append1 append2");
     }
 }
