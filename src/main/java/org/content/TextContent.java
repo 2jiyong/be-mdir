d02d2de2809f0dd2e20d76b36229248ed46b7ed3
2jiyong <jdragon2000@naver.com>
2025-03-11 14:10:26 +0900

    Feat: TextContent 객체와 테스트 생성

---
+++ src/main/java/org/content/TextContent.java
@@ -0,0 +1,19 @@
+package org.content;
+
+public class TextContent {
+    private String text;
+
+    public TextContent() {}
+
+    public TextContent(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
