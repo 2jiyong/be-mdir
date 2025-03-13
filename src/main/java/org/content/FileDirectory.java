547076bab2938511dcb5ebf51ec6ed20e7e80737
2jiyong <jdragon2000@naver.com>
2025-03-13 11:43:45 +0900

    Feat, Test: 부모 디렉토리 관리

--- src/main/java/org/content/FileDirectory.java
+++ src/main/java/org/content/FileDirectory.java
@@ -3,26 +3,31 @@ package org.content;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class FileDirectory {
+    private static final String ROOT_DIRECTORY_PATH = "C:\\";
+    private static final String PARENT_PATH = "/..";
     private File directory;
-    private static final File rootDirectory = new File("C://");
+    private Optional<File> parentDirectory;
     private final List<File> directories;
     private final List<File> files;
 
     public FileDirectory() {
-        this.directory = new File("C://");
+        parentDirectory = Optional.empty();
         directories = new ArrayList<>();
         files = new ArrayList<>();
+        setDirectory(getRootDirectory());
     }
 
     public void setDirectory(File directory) {
-        if(directory.exists()) return;
+        if(!directory.exists()) return;
         this.directory = directory;
         getFilesAndDirectories();
     }
 
     public void printFiles() {
+        System.out.print(getParentString());
         System.out.print(getDirectoriesString());
         System.out.print(getFilesString());
     }
@@ -31,13 +36,21 @@ public class FileDirectory {
         File[] directoryFiles = directory.listFiles();
         directories.clear();
         files.clear();
+        if(!isRootDirectory(directory)) {
+            parentDirectory = Optional.of(directory.getParentFile());
+        }
+
         for(File file : directoryFiles) {
             if(file.isDirectory()) directories.add(file);
             else files.add(file);
         }
     }
 
-    private String getDirectoriesString(){
+    public String getParentString(){
+        return isRootDirectory(directory) ? "" : PARENT_PATH+"\n";
+    }
+
+    public String getDirectoriesString(){
         StringBuilder stringBuilder = new StringBuilder();
         for(File file : directories) {
             stringBuilder.append("/");
@@ -47,12 +60,20 @@ public class FileDirectory {
         return stringBuilder.toString();
     }
 
-    private String getFilesString(){
+    public String getFilesString(){
         StringBuilder stringBuilder = new StringBuilder();
         for(File file : files) {
             stringBuilder.append(file.getName());
             stringBuilder.append("\n");
         }
         return stringBuilder.toString();
+    }
+
+    private boolean isRootDirectory(File file) {
+        return file.getAbsolutePath().equals(ROOT_DIRECTORY_PATH);
+    }
+
+    private File getRootDirectory() {
+        return new File(ROOT_DIRECTORY_PATH);
     }
 }
--- src/test/java/org/content/FileDirectoryTest.java
+++ src/test/java/org/content/FileDirectoryTest.java
@@ -15,9 +15,18 @@ public class FileDirectoryTest {
     }
 
     @Test
-    @DisplayName("FileDirectory 객체의 경로를 설정하고, 객체의 디렉토리와 파일을 출력할 수 있다.")
+    @DisplayName("FileDirectory 객체를 생성하고 초기값으로 초기화 할 수 있다.")
+    void 객체_초기화_생성(){
+        fileDirectory = new FileDirectory();
+        fileDirectory.printFiles();
+    }
+
+    @Test
+    @DisplayName("FileDirectory 객체의 경로를 설정하고, 객체의 부모, 디렉토리와 파일을 출력할 수 있다.")
     void 객체_생성_및_출력() {
         fileDirectory.setDirectory(new File("C://Users/hulli/Desktop/be-mdir/files"));
-        fileDirectory.printFiles();
+        assertThat(fileDirectory.getParentString()).isEqualTo("/..\n" );
+        assertThat(fileDirectory.getDirectoriesString()).isEqualTo("/direc\n" );
+        assertThat(fileDirectory.getFilesString()).isEqualTo("hello.txt\n");
     }
 }
